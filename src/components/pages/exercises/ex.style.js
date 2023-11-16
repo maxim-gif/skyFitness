39b4ce310f949ebabc69165072617aa47fa58d8e
dmitriy <admin@silverboats.ru>
2023-11-16 16:51:45 +0300

    fix

--- src/components/pages/exercises/ex.style.js
+++ src/components/pages/exercises/ex.style.js
@@ -76,3 +76,58 @@ export const NamePass = styled.div`
   padding-top: 40px;
   gap: 20px;
 `;
+
+export const ButtonS = styled.button`
+  border-radius: 46px;
+  background: var(--palette-purple-90, #580ea2);
+  width: 275px;
+  height: 52px;
+  flex-shrink: 0;
+  color: #fff;
+  font-variant-numeric: lining-nums proportional-nums;
+  font-family: StratosSkyeng;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 24px; /* 133.333% */
+  letter-spacing: -0.05px;
+  margin-top: 20px;
+
+  &:hover {
+    background: #2d1f79;
+  }
+
+  &:active {
+    background: #3b29a1;
+  }
+`;
+
+export const MyCoursesW = styled.h1`
+color: #000;
+font-variant-numeric: lining-nums proportional-nums;
+font-family: StratosSkyeng;
+font-size: 48px;
+font-style: normal;
+font-weight: 400;
+line-height: 56px; /* 116.667% */
+padding-top: 91px;
+`;
+
+export const NameCourse = styled.p`
+color: #000;
+
+font-variant-numeric: lining-nums proportional-nums;
+font-family: Abhaya Libre ExtraBold;
+font-size: 36px;
+font-style: normal;
+font-weight: 800;
+line-height: 110%; /* 39.6px */
+letter-spacing: -0.5px;
+`;
+
+export const MyCourses = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    gap: 40px;
+    padding-top: 60px;
+`;
--- src/components/pages/exercises/index.js
+++ src/components/pages/exercises/index.js
@@ -3,15 +3,25 @@ import * as S from "./ex.style";
 import { useSelector } from "react-redux";
 import { signOut } from "@firebase/auth";
 
+
 export const Profile = () => {
   const user = useSelector((state) => state.playerControl.dataUser);
   const courseNameMapping = {
+    bodyflex: './bodyflex.svg',
+    dancefitness: './dancefitness.svg',
+    stepaerobics: './stepaerobics.svg',
+    stretching: './stretching.svg',
+    yoga: './yoga.svg',
+  };
+
+  const courseTitleMapping = {
     bodyflex: 'Бодифлекс',
-    dancefitness: 'Фитнес-танцы',
+    dancefitness: 'Танцевальная фитнес',
     stepaerobics: 'Степ-аэробика',
     stretching: 'Стретчинг',
     yoga: 'Йога',
   };
+
 
   const exit = () => {
     signOut()
@@ -22,7 +32,6 @@ export const Profile = () => {
 
   return (
     <S.Wrapper>
-      <p>Привет </p>
       <S.LogoHeader>
         <NavLink to="/">
           <S.Logo src="/logo2.png"></S.Logo>
@@ -35,12 +44,24 @@ export const Profile = () => {
       <S.NamePass>
         <S.SpanName>Мой логин: {user.email}</S.SpanName>
         <S.SpanName>Пароль: {user.password}</S.SpanName>
+        <S.ButtonS>Редактировать логин</S.ButtonS>
+        <S.ButtonS>Редактировать пароль</S.ButtonS>
       </S.NamePass>
-      <p> Ваши курсы: </p>
+      <S.MyCoursesW>Мои курсы</S.MyCoursesW>
+      <S.MyCourses>
       {Object.keys(user.courses).map((courseKey) => {
-    const courseName = courseNameMapping[courseKey] || courseKey;
-    return <p key={courseKey}>{courseName}</p>
+  const courseSvg = courseNameMapping[courseKey];
+  const courseTitle = courseTitleMapping[courseKey];
+  return (
+
+    <div key={courseKey}>
+        <S.NameCourse>{courseTitle}</S.NameCourse>
+        <img src={courseSvg} alt={courseTitle}/>
+    </div>
+
+  )
 })}
+    </S.MyCourses>
     </S.Wrapper>
   );
 };
