07d7c9571256a0af521c540a04a7e4639a2fb191
dmitriy <admin@silverboats.ru>
2023-11-20 11:45:46 +0300

    fix

--- src/components/pages/profile/Profile.jsx
+++ src/components/pages/profile/Profile.jsx
@@ -1,90 +1,88 @@
-
 import { NavLink } from "react-router-dom";
 import * as S from "./Profile.styles";
 import { useEffect, useState } from "react";
 import { MenuProfile } from "../../menuProf/MenuProfile";
 import { ListLessons } from "../../listLesons";
 import { getUserLessons, auth, editPassword } from "../../api/api";
-import { onAuthStateChanged } from 'firebase/auth';
-import { useDispatch } from 'react-redux'
-import { signIn } from '../../store/actions/creators'
+import { onAuthStateChanged } from "firebase/auth";
+import { useDispatch } from "react-redux";
+import { signIn } from "../../store/actions/creators";
 
 export const ProfilePage = () => {
-
-	const name = window.localStorage.getItem("name");
+  const name = window.localStorage.getItem("name");
   const [color, setColor] = useState();
-	const dispatch = useDispatch()
-	const [isOpen, setIsOpen] = useState(false);
-	const toggleOpen = () => setIsOpen(!isOpen);
-	const [userLesson, setUserLesson] = useState(JSON.parse(window.localStorage.getItem("lesson")));
+  const dispatch = useDispatch();
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleOpen = () => setIsOpen(!isOpen);
+  const [userLesson, setUserLesson] = useState(
+    JSON.parse(window.localStorage.getItem("lesson"))
+  );
 
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        (async () => {
+          try {
+            const userData = await getUserLessons();
+            dispatch(signIn(userData));
+            setUserLesson(userData.courses);
+            console.log(userData);
+          } catch (error) {
+            console.log(error);
+          }
+        })();
+      } else {
+      }
+    });
 
-	useEffect(() => {
-		const unsubscribe = onAuthStateChanged(auth, (user) => {
-		  if (user) {
-			(async () => {
-			  try {
-				const userData = await getUserLessons();
-				dispatch(signIn(userData));
-				setUserLesson(userData.courses)
-				console.log(userData);
-			  } catch (error) {
-				console.log(error);
-			  }
-			})();
-		  } else {
-		  }
-		});
-	  
-		return () => unsubscribe();
-	  }, []);
+    return () => unsubscribe();
+  }, []);
 
+  useEffect(() => {
+    setColor(false);
+  }, []);
 
-	useEffect(() => {
-		setColor(false);
-	}, []);
+  function mouseOut() {
+    if (isOpen) {
+      toggleOpen();
+    }
+  }
 
-	function mouseOut() {
-		if (isOpen) {
-			toggleOpen();
-		}
-	}
+  return (
+    <S.Wrapper>
+      <S.LogoHeader>
+        <NavLink to="/">
+          <S.Logo src="/logo2.png" />
+        </NavLink>
 
-	return (
-		<S.Wrapper>
-			<S.LogoHeader>
-				<NavLink to='/'>
-					<S.Logo src='/logo2.png' />
-				</NavLink>
-
-				<S.MenuStyle onMouseLeave={mouseOut} onClick={toggleOpen}>
-					<svg
-						xmlns='http://www.w3.org/2000/svg'
-						width='50'
-						height='50'
-						viewBox='0 0 50 50'
-						fill='none'
-					>
-						<circle
-							cx='25'
-							cy='25'
-							r='25'
-							fill={color ? 'rgb(105, 105, 105)' : '#D9D9D9'}
-						/>
-					</svg>
-					<S.SpanName $color={color}>{name}</S.SpanName>
-					<MenuProfile color={color} isOpen={isOpen} />
-				</S.MenuStyle>
-			</S.LogoHeader>
-			<S.MyProf>Мой профиль</S.MyProf>
-			<S.NamePass>
-				<S.SpanNameFull>Логин: {name}</S.SpanNameFull>
-				{/* <S.SpanName>Пароль: {user.password}</S.SpanName> */}
-				<S.ButtonS>Редактировать логин</S.ButtonS>
-				<S.ButtonS>Редактировать пароль</S.ButtonS>
-			</S.NamePass>
-			<S.MyCoursesW>Мои курсы</S.MyCoursesW>
-			<ListLessons userLesson={userLesson}/>
-		</S.Wrapper>
-	);
+        <S.MenuStyle onMouseLeave={mouseOut} onClick={toggleOpen}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="50"
+            height="50"
+            viewBox="0 0 50 50"
+            fill="none"
+          >
+            <circle
+              cx="25"
+              cy="25"
+              r="25"
+              fill={color ? "rgb(105, 105, 105)" : "#D9D9D9"}
+            />
+          </svg>
+          <S.SpanName $color={color}>{name}</S.SpanName>
+          <MenuProfile color={color} isOpen={isOpen} />
+        </S.MenuStyle>
+      </S.LogoHeader>
+      <S.MyProf>Мой профиль</S.MyProf>
+      <S.NamePass>
+        <S.SpanNameFull>Логин: {name}</S.SpanNameFull>
+        {/* <S.SpanName>Пароль: {user.password}</S.SpanName> */}
+        <S.ButtonS>Редактировать логин</S.ButtonS>
+        <S.ButtonS>Редактировать пароль</S.ButtonS>
+      </S.NamePass>
+      <S.MyCoursesW>Мои курсы</S.MyCoursesW>
+      <ListLessons userLesson={userLesson} />
+    </S.Wrapper>
+  );
 };
